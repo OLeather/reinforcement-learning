063018ac835bcd2110f74a205ca98810281f482b
Owen Leather <owenleather03@gmail.com>
2023-06-18 16:45:31 -0400

    added deep q learning

---
+++ deep-q-learning.py
@@ -0,0 +1,209 @@
+import gym
+import math
+import numpy as np
+import random
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import animation
+matplotlib.use('tkagg')
+from itertools import count
+
+from collections import namedtuple, deque
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+    
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+    
+class DQN(nn.Module):
+
+    def __init__(self, state_size, action_size):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(state_size, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, action_size)
+
+    def forward(self, x):
+        # print(x, self.layer1)
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+steps_done = 0
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threashold = EPS_END + (EPS_START-EPS_END) * math.exp(-1 * steps_done / EPS_DECAY)
+    steps_done += 1
+    if(sample > eps_threashold):
+        with torch.no_grad():
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
+
+episode_durations = []
+episode_rewards = []
+def plot_durations(show_result=False):
+    plt.figure(1)
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    rewards_t = torch.tensor(episode_rewards, dtype=float)
+    if show_result:
+        plt.title('Result')
+    else:
+        plt.clf()
+        plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Reward')
+    plt.plot(rewards_t.numpy())
+
+    if len(rewards_t) >= 100:
+        means = rewards_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.pause(0.001)
+    
+def optimize_model():
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+    batch = Transition(*zip(*transitions))
+
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    with torch.no_grad():
+        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0]
+
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+    
+    optimizer.zero_grad()
+    loss.backward()
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    optimizer.step()
+
+    
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+TAU = 0.005
+LR = 1e-4
+
+env = gym.make('CartPole-v1', render_mode='rgb_array')
+
+state, info = env.reset()
+
+state_size = len(state)
+action_size = env.action_space.n
+
+
+policy_net = DQN(state_size, action_size).to(device)
+target_net = DQN(state_size, action_size).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+
+optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+memory = ReplayMemory(10000)
+
+num_episodes = 600
+
+def train():
+
+    for i_episode in range(num_episodes):
+        print("episode:", i_episode)
+        state, info = env.reset()
+        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+        reward_sum = 0
+        for t in count():
+            action = select_action(state)
+            observation, reward_, terminated, truncated, _ = env.step(action.item())
+            reward = torch.tensor([reward_], device=device)
+            done = terminated or truncated
+            reward_sum += reward_
+
+            if terminated:
+                next_state = None
+            else:
+                next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+
+            memory.push(state, action, next_state, reward)
+
+            state = next_state
+
+            optimize_model()
+
+            target_net_state_dict = target_net.state_dict()
+            policy_net_state_dict = policy_net.state_dict()
+            for key in policy_net_state_dict:
+                target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
+            
+            target_net.load_state_dict(target_net_state_dict)
+
+            if done:
+                episode_durations.append(t+1)
+                episode_rewards.append(reward_sum)
+                plot_durations()
+                break
+
+
+
+train()
+
+print("Complete")
+# plot_durations(show_result = True)
+# plot.show()
+
+state, info = env.reset()
+state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+
+fig = plt.figure()
+img = plt.imshow(env.render())
+
+def test(i): 
+    print("visualizing:", i)
+    global state
+    action = select_action(state)
+    observation, reward_, terminated, truncated, _ = env.step(action.item())
+    reward = torch.tensor([reward_], device=device)
+    next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+    memory.push(state, action, next_state, reward)
+
+    state = next_state
+    img.set_data(env.render())
+    return img
+steps = 100
+anim = animation.FuncAnimation(fig, test, frames=steps,
+                            interval=100)
+plt.show()
+
